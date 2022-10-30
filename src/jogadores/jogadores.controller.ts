701ffd26b21f95b44171b89404d29c012f635094
Adriel Modollo <68829816+AdrielModollo@users.noreply.github.com>
2022-10-30 00:03:52 -0300

    feat: buscar jogador pelo email

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface'
+import { query } from 'express';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -15,8 +16,12 @@ export class JogadoresController {
     }
 
     @Get()
-    async consultarJogadores(): Promise<Jogador[]> {
-        return this.jogadoresService.consultarTodosJogadores();
+    async consultarJogadores(@Query('email') email: string): Promise<Jogador[] | Jogador> {
+        if (email) {
+            return await this.jogadoresService.consultarJogadorPeloEmail(email)
+        } else {
+            return await this.jogadoresService.consultarTodosJogadores();
+        }
     }
 
 }
